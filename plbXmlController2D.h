879a7b5c17637907e024df5c30cca47d0cb4399e
Philippe Seil <philippe.seil@jku.at>
2012-06-11 10:00:22 +0200

    initial commit
    this is the initial commit of the XML scripting interface for Palabos

---
+++ plbXmlController2D.h
@@ -0,0 +1,14 @@
+/*
+ * plbXmlController2D.h
+ * provides an xml interface to palabos
+ */
+
+template<typename T>
+class PlbXmlController2D {
+public:
+  PlbXmlController2D() : i(1) {};
+  ~PlbXmlController2D() {};
+
+private:
+  int i;
+};
---
+++ xmlMain.cpp
@@ -0,0 +1,16 @@
+/*
+ * main file to test functions of plbXmlController2D
+ */
+
+#include "palabos2D.h"
+#include "palabos2D.hh"
+
+#include "plbXmlController2D.h"
+
+int main(int argc, char **argv)
+{
+  plb::plbInit(&argc,&argv);
+
+
+  return 0;
+}
